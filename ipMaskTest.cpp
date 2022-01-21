2a0acb38a65624a4f6801580ca5193f38c55cc3e
janny00kevin <janny00kevin@gmail.com>
2022-01-21 18:58:00 +0800

    can't deliver lower than 3 arguments

--- ipMaskTest.cpp
+++ ipMaskTest.cpp
@@ -16,11 +16,14 @@ vector<int> transStr2Int(vector<string>& vStr)
         ss << vStr.at(i);
         ss >> tmp;
         vInt.push_back(tmp);
+
+        ss.str("");
+        ss.clear();
     }
     return vInt;
 }
 
-vector<int> andOperator(vector<int>& ipVect, vector<int> mask){
+vector<int> andOp4vect(vector<int>& ipVect, vector<int> mask){
     vector<int> v;
     try
     {
@@ -29,7 +32,7 @@ vector<int> andOperator(vector<int>& ipVect, vector<int> mask){
     catch(int x)
     {
         cerr << "Ip size and mask size doesn't match!\n";
-        // return
+        return v;
     }
 
     for (size_t i = 0; i < mask.size(); i++)
@@ -40,10 +43,10 @@ vector<int> andOperator(vector<int>& ipVect, vector<int> mask){
     return v;
 }
 
-bool ipMask(string ipChar1 = "192.168.0.1", string ipChar2 = "192.168.1.2")
+bool ipMaskCompare(string ipChar1 = "192.168.0.1", string ipChar2 = "192.168.1.2", string mask = "255.255.255.0")
 {
     string ip1 = ipChar1, ip2 = ipChar2;
-    string mask = "255.255.255.0";
+    ;
     vector<string> vStr1, vStr2, vMaskStr;
     vStr1 = split(ip1, ".");
     vStr2 = split(ip2, ".");
@@ -53,19 +56,34 @@ bool ipMask(string ipChar1 = "192.168.0.1", string ipChar2 = "192.168.1.2")
     ipVect2 = transStr2Int(vStr2);
     maskVect = transStr2Int(vMaskStr);
     vector<int> v1, v2;
-    v1 = andOperator(ipVect1, maskVect);
-    v2 = andOperator(ipVect2, maskVect);
+    v1 = andOp4vect(ipVect1, maskVect);
+    v2 = andOp4vect(ipVect2, maskVect);
+    // for (size_t i = 0; i < vStr1.size(); i++)
+    // {
+    //     cout << "v1: " << ipVect1.at(i) << ", v2: " << ipVect2.at(i) << endl;
+    // }
+    
     if (v1 == v2)  return 1;
     else  return 0;
 }
 
 int main(int argc, char *argv[])
 {
-    //if (argc == 0)
-        if(ipMask())
+    // cout << argv[0];
+    // if (argc == 1)
+    //     if (ipMaskCompare())
+    //         cout << "these 2 ips are in the same subnetwork.\n";
+    //     else
+    //         cout << "these 2 ips are NOT in the same subnetwork.\n";
+    // else if(argc == 3)
+        if (ipMaskCompare(argv[1], argv[2], argv[3]))
             cout << "these 2 ips are in the same subnetwork.\n";
         else
             cout << "these 2 ips are NOT in the same subnetwork.\n";
+
+        
+        
+        
 
     return 0;
 }
